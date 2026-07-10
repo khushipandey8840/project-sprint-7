6c8aff3c62a255478016aae2a72b4154fc43989e
SAKSHI PANDEY <khushipandey9549@gmail.com>
2026-07-11 00:36:44 +0530

    Delete src/formSchema.js

--- src/formSchema.js
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const formSchema = z.object({
-  name: z.string().min(2, "Name must be at least 2 characters"),
-  email: z.string().email("Enter a valid email"),
-  username: z.string().min(3, "Username must be at least 3 characters"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
-});
